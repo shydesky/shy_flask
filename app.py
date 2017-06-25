489cbb55eed05beb49cbb21e7b251d710a9c0cda
shy <sunhaoyu@papayamobile.cn>
2017-06-25 19:57:21 +0800

    s

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
+import os
 from flask import Flask
-from extensions import api
+from flask.helpers import get_debug_flag
+from extensions import api, db
 from shy_flask import (module1)
-app = Flask(__name__)
-app.config.from_object('config')
-api.init_app(app)
+from settings import ProdConfig, DevConfig
+
+
+def create_app(config_object=ProdConfig):
+    app = Flask(__name__)
+    app.config.from_object('config')
+    register_extensions(app)
+    return app
+
+
+def which_config():
+    u"""在环境变量中导出USE_CONFIG决定使用哪个配置."""
+    config_dict = {
+        'prod': ProdConfig,
+        'dev': DevConfig,
+    }
+    environ = os.environ.get('USE_CONFIG', 'dev')
+    config = config_dict.get(environ)
+    is_debug = get_debug_flag()
+    config = (config or DevConfig) if is_debug else config
+    return config
+
+config = which_config()
+app = create_app(config_object=config)
+
+
+def register_extensions(app):
+    api.init_app(app)
+    db.init_app(app)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- extensions.py
+++ extensions.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import types
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 api = Api()
+db = SQLAlchemy()
+
 
 def api_route(self, *args, **kwargs):
-    """ 给flask api加上route功能，避免直接调用api.add_resource函数了.
-    具体请参考crm_backend.employee.api里的使用
-    http://flask.pocoo.org/snippets/129/
+    u"""给flask api加上route功能，避免直接调用api.add_resource函数了.
+
+    参考: http://flask.pocoo.org/snippets/129/
     """
     def wrapper(cls):
         self.add_resource(cls, *args, **kwargs)
