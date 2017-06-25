325aa1a0bb4eb93c682854c9a17d1079b2146d14
shy <sunhaoyu@papayamobile.cn>
2017-06-25 17:22:48 +0800

    l

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from extensions import api
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+api.init_app(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- module1/api.py
+++ module1/api.py
@@ -2,6 +2,7 @@ from extensions import api
 from flask_restful import Resource
 import pdb;pdb.set_trace()
 
+
 @api.route('api/module1')
 class moduleApi(Resource):
 
